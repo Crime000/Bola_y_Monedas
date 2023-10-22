9bbe418d606d269c2af54d4175bd2dfdc40c8ff1
Crime000 <amgd.skellington@gmail.com>
2023-10-22 23:50:25 +0100

    Cambios del fin de semana

--- Assets/Scripts/Camara2.cs
+++ Assets/Scripts/Camara2.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Camara2 : MonoBehaviour
@@ -8,6 +9,9 @@ public class Camara2 : MonoBehaviour
     public GameObject esfera;
     public float offSetY, offSetZ;
     public bool primeraPersona = false;
+    private float x, y;
+    public float sensibilidad = 1f;
+    private Vector3 rotar;
 
     void Update()
     {
@@ -26,11 +30,19 @@ public class Camara2 : MonoBehaviour
         {
             //camara primera persona.
             transform.position = esfera.transform.position;
+            y = 5 * -Input.GetAxis("Mouse X");
+            x = 5 * Input.GetAxis("Mouse Y");
+            rotar = new Vector3(x, y * sensibilidad, 0f);
+            transform.eulerAngles = transform.eulerAngles - rotar;
         }
         else
         {
             //camara tercera persona
             transform.position = new Vector3(esfera.transform.position.x, esfera.transform.position.y + offSetY, esfera.transform.position.z - offSetZ);
+            y = 5 * -Input.GetAxis("Mouse X");
+            x = 5 * Input.GetAxis("Mouse Y");
+            rotar = new Vector3(x, y * sensibilidad, 0f);
+            transform.eulerAngles = transform.eulerAngles - rotar;
         }
     }
 }
